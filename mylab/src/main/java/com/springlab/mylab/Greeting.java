66ca1ff63cab72ea59b8025265134268b0c16f13
Jenny Tang <jennydtang@gmail.com>
2020-06-18 17:08:58 -0400

    Change story form

--- mylab/src/main/java/com/springlab/mylab/Greeting.java
+++ mylab/src/main/java/com/springlab/mylab/Greeting.java
@@ -3,6 +3,9 @@ package com.springlab.mylab;
 public class Greeting {
     private long id;
     private String content;
+    private String verb;
+    private String animal;
+    private String place;
 
     public long getId() {
         return id;
@@ -18,5 +21,29 @@ public class Greeting {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public String getVerb() {
+        return verb;
+    }
+
+    public void setVerb(String verb) {
+        this.verb = verb;
+    }
+
+    public String getAnimal() {
+        return animal;
+    }
+
+    public void setAnimal(String animal) {
+        this.animal = animal;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
     }
 }
